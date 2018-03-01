354b565161ccc04b13a4b0d856064c1ba5adfd52
smandal85 <joyrocks84@gmail.com>
2018-03-01 13:18:45 +0100

    "Re-factored test class and cleaned structure"

--- src/test/java/com/atmlocator/controllers/test/AtmLocatorControllerTest.java
+++ src/test/java/com/atmlocator/controllers/test/AtmLocatorControllerTest.java
@@ -1,9 +1,8 @@
 package com.atmlocator.controllers.test;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
@@ -13,14 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.atmlocator.AtmLocatorApplication;
 import com.atmlocator.controllers.AtmLocatorController;
@@ -39,9 +37,6 @@ public class AtmLocatorControllerTest {
 	@InjectMocks
 	private AtmLocatorController atmLocatorController;
 
-	@Autowired
-	private WebApplicationContext webApplicationContext;
-
 	@Mock
 	private AtmLocatorService atmLocatorService;
 
@@ -49,10 +44,6 @@ public class AtmLocatorControllerTest {
 	public void init() {
 		MockitoAnnotations.initMocks(this);
 		mockMvc = MockMvcBuilders.standaloneSetup(atmLocatorController).build();
-	}
-
-	@Test
-	public void test_get_all_success() throws Exception {
 
 		GeoLocation geoLocation1 = new GeoLocation("52.36139", "4.939421");
 		Address address1 = new Address("Insulindeweg", "478", "1094 MH", "Amsterdam", geoLocation1);
@@ -65,10 +56,17 @@ public class AtmLocatorControllerTest {
 		AtmDetails[] atmDetailsList = new AtmDetails[] { atmDetails1, atmDetails2 };
 
 		Mockito.when(atmLocatorService.getAllAtms()).thenReturn(atmDetailsList);
-
-		mockMvc.perform(get("/all-atms"))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$", hasSize(2)));
 	}
+
+	@Test
+	public void test_get_all_success() throws Exception {
+
+		mockMvc.perform(get("/all-atms")).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect(jsonPath("$", hasSize(2)))
+				.andExpect(jsonPath("$[0].address.street", is("Insulindeweg")))
+				.andExpect(jsonPath("$[0].address.postalcode", is("1094 MH")))
+				.andExpect(jsonPath("$[0].type", is("ING"))).andExpect(jsonPath("$[1].address.street", is("De Beurs")))
+				.andExpect(jsonPath("$[1].address.city", is("Amersfoort"))).andDo(MockMvcResultHandlers.print());
+	}
+
 }
