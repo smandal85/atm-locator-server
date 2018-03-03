564dc0dd6071fa317c418be227e81909e01ee765
smandal85 <joyrocks84@gmail.com>
2018-03-03 19:47:35 +0100

    "Minor changes"

--- src/main/java/com/atmlocator/controllers/AtmLocatorController.java
+++ src/main/java/com/atmlocator/controllers/AtmLocatorController.java
@@ -3,9 +3,9 @@ package com.atmlocator.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.atmlocator.domain.AtmDetails;
 import com.atmlocator.services.AtmLocatorService;
@@ -16,14 +16,14 @@ import com.atmlocator.services.AtmLocatorService;
  *         This is the Controller class for the application.Accepts the URL from
  *         the client (/all-atms).Returns a ResponseEntity list of AtmDetails
  */
-@Controller
+@RestController
 @CrossOrigin(origins = { "http://localhost:4200" })
 public class AtmLocatorController {
 
 	@Autowired
 	private AtmLocatorService atmLocatorService;
 
-	@GetMapping("all-atms")
+	@RequestMapping("all-atms")
 	public ResponseEntity<AtmDetails[]> getAllAtmsList() {
 
 		System.out.println("*** Inside method AtmLocatorController.getAllAtmsList() ***");
